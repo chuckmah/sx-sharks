5503a1f78393924fac4ccb67b51ac4c71f95916f
Charles Maheu <choc.mah@gmail.com>
2025-05-09 09:23:00 -0400

    ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+        {
+            name: 'website',
+            script: 'npm run start',
+        },
+        {
+            name: 'updater',
+            script: 'npm run updater',
+        },
+    ],
+};
